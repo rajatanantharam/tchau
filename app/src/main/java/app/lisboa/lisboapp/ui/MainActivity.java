90869f5eacde42bba29950228f3d32231811d77a
Rajat Anantharam <rajat@funda.nl>
2016-11-01 17:27:22 +0100

    Configured with firebase and also added reference SDK

---
+++ app/src/main/java/app/lisboa/lisboapp/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package app.lisboa.lisboapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Rajat Anantharam on 01/11/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
