f328b2cdd8a633573994ae289c08744c2cdf0c25
Rajat Anantharam <rajat@funda.nl>
2016-11-02 21:16:18 +0100

    POC - google place picker widget

--- app/src/main/java/app/lisboa/lisboapp/ui/NewEventActivity.java
+++ app/src/main/java/app/lisboa/lisboapp/ui/NewEventActivity.java
@@ -1,10 +1,15 @@
 package app.lisboa.lisboapp.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,6 +24,8 @@ import app.lisboa.lisboapp.model.Event;
 public class NewEventActivity extends BaseLocationActivity {
 
     private EditText host, eventType, eventLocation, duration;
+    private static final int PLACE_PICKER_REQUEST = 1;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,27 +38,44 @@ public class NewEventActivity extends BaseLocationActivity {
 
     }
 
-    public void broadCastEvent(View view) {
+    public void broadCastEvent(View view) throws GooglePlayServicesNotAvailableException, GooglePlayServicesRepairableException {
 
-        if(host.getText() == null || host.getText().toString().equalsIgnoreCase("")
-                || eventType.getText() == null || eventType.getText().toString().equalsIgnoreCase("")
-                || eventLocation.getText() == null || eventLocation.getText().toString().equalsIgnoreCase("")
-                || duration.getText() == null || duration.getText().toString().equalsIgnoreCase("")) {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
 
-            Toast.makeText(this,"Please fill up all the fields",Toast.LENGTH_SHORT).show();
-            return;
+
+        if(false) {
+
+            if (host.getText() == null || host.getText().toString().equalsIgnoreCase("")
+                    || eventType.getText() == null || eventType.getText().toString().equalsIgnoreCase("")
+                    || eventLocation.getText() == null || eventLocation.getText().toString().equalsIgnoreCase("")
+                    || duration.getText() == null || duration.getText().toString().equalsIgnoreCase("")) {
+
+                Toast.makeText(this, "Please fill up all the fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            DatabaseReference eventsRef = FirebaseDatabase.getInstance().getReference().child("events");
+
+            Double lat = 0D, lon = 0D;
+            if (mCurrentLocation != null && mCurrentLocation.hasAccuracy()) {
+                lat = mCurrentLocation.getLatitude();
+                lon = mCurrentLocation.getLongitude();
+            }
+
+            eventsRef.push().setValue(new Event(host.getText().toString(), eventType.getText().toString(), eventLocation.getText().toString(), lat, lon,
+                    new Date().getTime(), Integer.parseInt(duration.getText().toString())));
+            finish();
         }
+    }
 
-        DatabaseReference eventsRef = FirebaseDatabase.getInstance().getReference().child("events");
-
-        Double lat = 0D, lon = 0D;
-        if(mCurrentLocation != null && mCurrentLocation.hasAccuracy()) {
-            lat = mCurrentLocation.getLatitude();
-            lon = mCurrentLocation.getLongitude();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this,data);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
         }
-
-        eventsRef.push().setValue(new Event(host.getText().toString(),eventType.getText().toString(),eventLocation.getText().toString(),lat,lon,
-                new Date().getTime(),Integer.parseInt(duration.getText().toString())));
-        finish();
     }
 }
